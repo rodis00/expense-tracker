037383c6d133de9676ab7915814a25c7f6df4d48
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-01-14 22:00:58 +0100

    adding routes and links to different components

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,22 +1,36 @@
 import React from "react";
 import classes from "./Nav.module.css";
-
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import Expenses from "../Expenses/Expenses";
+import Earnings from "../Earnings/Earnings";
+import Home from "../Home/Home";
+import Summaries from "../Summaries/Summaries"
 
 function Nav() {
-
-  const navElements = [`Home`, "Expenses", "Earnings", "Summaries"];
+  const navElements = ["Expenses", "Earnings", "Summaries"];
 
   return (
-    <nav className={`${classes.navbar}`}>
-      <h2 className={classes.navbar__h2}>A&A</h2>
-      <ul className={classes.navbar__list}>
-        {navElements.map(elem=>(
-          <li key={elem} className={classes.navbar__list__elem}>{elem}</li>
-        ))}
-      </ul>
-      <span className={classes.navbar__span}></span>
-      <button className={classes.navbar__btn}>Login</button>
-    </nav>
+    <Router>
+      <nav className={`${classes.navbar}`}>
+        <h2 className={classes.navbar__h2}>A&A</h2>
+        <ul className={classes.navbar__list}>
+          <li key="Home" className={classes.navbar__list__elem}><Link to='/' className={classes.navbar__list__elem__link}>Home</Link></li>
+          {navElements.map((elem) => (
+            <li key={elem} className={classes.navbar__list__elem}>
+              <Link to={elem} className={classes.navbar__list__elem__link}>{elem}</Link>
+            </li>
+          ))}
+        </ul>
+        <span className={classes.navbar__span}></span>
+        <button className={classes.navbar__btn}>Login</button>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="earnings" element={<Earnings />} />
+        <Route path="summaries" element={<Summaries />} />
+      </Routes>
+    </Router>
   );
 }
 
