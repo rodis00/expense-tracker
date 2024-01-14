d00239776bb2bf0af690ad54bc0fdda4f242b5f4
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-01-14 21:23:08 +0100

    importing expenses component with moved code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,11 @@
 import "./App.css";
+import Expenses from "./components/Expenses/Expenses";
 import Nav from "./components/Nav/Nav";
 function App() {
   return (
     <>
       <Nav />
-      <main>
-        <form action="">
-          <label htmlFor="">Expense</label>
-          <input type="text" />
-          <label htmlFor="">Amount</label>
-          <input type="number" name="" id="" />
-          <label htmlFor="">Date</label>
-          <input type="date" />
-          <button type="submit">Submit</button>
-        </form>
-      </main>
+      <Expenses />
     </>
   );
 }
