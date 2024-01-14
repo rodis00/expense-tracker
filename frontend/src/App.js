e859580c1c5dd787caa9eb7a77828e014881680f
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-01-14 22:00:24 +0100

    cleaning app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import Expenses from "./components/Expenses/Expenses";
 import Nav from "./components/Nav/Nav";
 function App() {
   return (
     <>
       <Nav />
-      <Expenses />
     </>
   );
 }
