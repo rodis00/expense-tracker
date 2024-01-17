ac4aed44bf94bc54dc585930bec00e0aa95e5833
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-01-17 21:16:14 +0100

    deleting unused stuff and adding state to manage menu visibility

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Nav.module.css";
 import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
 import Expenses from "../Expenses/Expenses";
@@ -9,35 +9,58 @@ import Button from "../Button/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
+import { icon } from "@fortawesome/fontawesome-svg-core";
 
 function Nav() {
+  const [menuActive, setMenuActive] = useState(false);
+
   const navElements = [
+    { icon: <FontAwesomeIcon icon={faHouse} />, title: "Home" },
     { icon: <FontAwesomeIcon icon={faDollarSign} />, title: "Expenses" },
     { icon: <FontAwesomeIcon icon={faDollarSign} />, title: "Earnings" },
     { icon: <FontAwesomeIcon icon={faDollarSign} />, title: "Summaries" },
   ];
 
+  function handleMenuActiveChange() {
+    setMenuActive((elem) => !elem);
+  }
+
   return (
     <Router>
       <nav className={classes.navbar}>
-        <h2 className={classes.navbar__h2}>A&A</h2>
+        {menuActive ? (
+          <h2 className={classes.navbar__h2}>A&A</h2>
+        ) : (
+          <button onClick={handleMenuActiveChange}>click</button>
+        )}
         <ul className={classes.navbar__list}>
-          <li key="Home" className={`${classes.navbar__list__elem} `}>
-            <Link to="/" className={classes.navbar__list__elem__link}>
-              <FontAwesomeIcon icon={faHouse} className={classes.navbar__list__elem__link__icon}/>
-              Home
-            </Link>
-          </li>
-          {navElements.map((elem) => (
-            <li key={elem.title} className={classes.navbar__list__elem}>
-              <Link to={`/${elem.title}`} className={classes.navbar__list__elem__link}>
-                <span className={classes.navbar__list__elem__link__icon}>{elem.icon}</span>
-                <span>{elem.title}</span>
-              </Link>
-            </li>
-          ))}
+          {navElements.map((elem) => {
+            return menuActive ? (
+              <li key={elem.title} className={classes.navbar__list__elem}>
+                <Link
+                  to={`/${elem.title}`}
+                  className={classes.navbar__list__elem__link}
+                >
+                  <span className={classes.navbar__list__elem__link__icon}>
+                    {elem.icon}
+                  </span>
+                  <span>{elem.title}</span>
+                </Link>
+              </li>
+            ) : (
+              <li key={elem.title} className={classes.navbar__list__elem}>
+                <Link
+                  to={`/${elem.title}`}
+                  className={classes.navbar__list__elem__link}
+                >
+                  <span className={classes.navbar__list__elem__link__icon}>
+                    {elem.icon}
+                  </span>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
-        <span className={classes.navbar__span}></span>
         <Button className={classes.navbar__btn}>Login</Button>
       </nav>
       <Routes>
