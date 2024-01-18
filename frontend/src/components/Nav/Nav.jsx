e09673f09055b64c751f37a26e2e02535abea095
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-01-18 21:55:00 +0100

    deleting unecessary elements to make code cleaner and adding stuff to manipulate menu better

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -5,14 +5,15 @@ import Expenses from "../Expenses/Expenses";
 import Earnings from "../Earnings/Earnings";
 import Home from "../Home/Home";
 import Summaries from "../Summaries/Summaries";
-import Button from "../Button/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
+import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import { icon } from "@fortawesome/fontawesome-svg-core";
 
 function Nav() {
   const [menuActive, setMenuActive] = useState(false);
+  const [chosenLink, setChosenLink] = useState("Home");
 
   const navElements = [
     { icon: <FontAwesomeIcon icon={faHouse} />, title: "Home" },
@@ -27,44 +28,51 @@ function Nav() {
 
   return (
     <Router>
-      <nav className={classes.navbar}>
-        {menuActive ? (
-          <h2 className={classes.navbar__h2}>A&A</h2>
-        ) : (
-          <button onClick={handleMenuActiveChange}>click</button>
-        )}
+      <button
+        onClick={handleMenuActiveChange}
+        className={`${classes.toogleNav} ${
+          menuActive ? classes.toogleActive : ""
+        }`}
+      ></button>
+      <nav
+        className={`${classes.navbar} ${
+          menuActive ? classes.navbarActive : ""
+        }`}
+      >
+        <h2>A&A</h2>
         <ul className={classes.navbar__list}>
-          {navElements.map((elem) => {
-            return menuActive ? (
-              <li key={elem.title} className={classes.navbar__list__elem}>
-                <Link
-                  to={`/${elem.title}`}
-                  className={classes.navbar__list__elem__link}
-                >
-                  <span className={classes.navbar__list__elem__link__icon}>
-                    {elem.icon}
-                  </span>
-                  <span>{elem.title}</span>
-                </Link>
-              </li>
-            ) : (
-              <li key={elem.title} className={classes.navbar__list__elem}>
-                <Link
-                  to={`/${elem.title}`}
-                  className={classes.navbar__list__elem__link}
-                >
-                  <span className={classes.navbar__list__elem__link__icon}>
-                    {elem.icon}
-                  </span>
-                </Link>
-              </li>
-            );
-          })}
+          <div className={classes.navbar__list__indicator}></div>
+          {navElements.map((elem) => (
+            <li key={elem.title} className={classes.navbar__list__elem}>
+              <Link
+                onClick={()=>setChosenLink(elem.title)}
+                to={`/${elem.title}`}
+                className={`${classes.navbar__list__elem__link} ${
+                  chosenLink === elem.title
+                    ? classes.navbar__list__elem__link__active
+                    : ""
+                }`}
+              >
+                <span className={classes.navbar__list__elem__link__icon}>
+                  {elem.icon}
+                </span>
+                <span className={classes.navbar__list__elem__link__title}>
+                  {elem.title}
+                </span>
+              </Link>
+            </li>
+          ))}
         </ul>
-        <Button className={classes.navbar__btn}>Login</Button>
+        <button className={classes.navbar__loginBtn}>
+          <FontAwesomeIcon
+            icon={faRightToBracket}
+            className={classes.navbar__loginBtn__icon}
+          />
+          <span className={classes.navbar__loginBtn__text}>Login</span>
+        </button>
       </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="Home" element={<Home />} />
         <Route path="expenses" element={<Expenses />} />
         <Route path="earnings" element={<Earnings />} />
         <Route path="summaries" element={<Summaries />} />
