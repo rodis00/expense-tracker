5bc94758fe3686fc8affa0677bafd659fb5162e0
rodis00 <happyekkov@gmail.com>
2024-01-19 23:04:09 +0100

    add earningDto

---
+++ backend/src/main/java/com/github/rodis00/backend/dto/EarningDto.java
@@ -0,0 +1,27 @@
+package com.github.rodis00.backend.dto;
+
+import com.github.rodis00.backend.model.Earning;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class EarningDto {
+    private Integer id;
+    private String title;
+    private Double amount;
+    private Date date;
+    private Integer userId;
+
+    public static EarningDto from (Earning earning) {
+        return new EarningDto(
+                earning.getId(),
+                earning.getTitle(),
+                earning.getAmount(),
+                earning.getDate(),
+                earning.getUser().getId()
+        );
+    }
+}
