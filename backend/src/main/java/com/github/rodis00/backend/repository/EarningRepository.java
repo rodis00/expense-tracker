eec960f41c8d719960074097b7ff57160e4122f1
rodis00 <happyekkov@gmail.com>
2024-01-20 00:22:12 +0100

    add findAllByUserId method to receive all user earnings and expenses

--- backend/src/main/java/com/github/rodis00/backend/repository/EarningRepository.java
+++ backend/src/main/java/com/github/rodis00/backend/repository/EarningRepository.java
@@ -4,6 +4,9 @@ import com.github.rodis00.backend.model.Earning;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EarningRepository extends JpaRepository<Earning, Integer> {
+    List<Earning> findAllByUserId(Integer userId);
 }
--- backend/src/main/java/com/github/rodis00/backend/repository/ExpenseRepository.java
+++ backend/src/main/java/com/github/rodis00/backend/repository/ExpenseRepository.java
@@ -4,6 +4,9 @@ import com.github.rodis00.backend.model.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
+    List<Expense> findAllByUserId(Integer userId);
 }
