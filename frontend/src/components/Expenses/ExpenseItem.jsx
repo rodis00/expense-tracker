ce1b0f8c97b1e2d8772336e767f51d8bd86ce377
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-01-20 20:03:59 +0100

    creating new components that help generate list of expenses conditionally

---
+++ frontend/src/components/Expenses/ExpenseItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExpenseItem() {
+  return (
+    <li>
+    
+    </li>
+  )
+}
+
+export default ExpenseItem
---
+++ frontend/src/components/Expenses/ExpenseList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExpenseList() {
+  return (
+    <ul>
+        
+    </ul>
+  )
+}
+
+export default ExpenseList
