eaf88f7924875de4374e7ccc6060372cea9649a1
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-01-20 20:05:29 +0100

    passing data to Expenses component that display other components and store data

--- frontend/src/components/Expenses/Expenses.jsx
+++ frontend/src/components/Expenses/Expenses.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import NewExpense from "./NewExpense";
 
 function Expenses() {
+  
+  const [expenses, setExpenses] = useState();
+
+  function handleNewExpense(expense){
+    setExpenses((prevExpense)=>{
+      return[expense, ...prevExpense];
+    })
+  }
+
   return (
-    <>
- 
-    </>
+    <div>
+      <NewExpense onAddExpense={handleNewExpense}/>
+    </div>
   );
 }
 
