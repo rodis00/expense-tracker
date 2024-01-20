b5ab69014d88aab9e4d73e75faef855e7da70627
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-01-20 19:22:13 +0100

    moving code from expenses to new component expensesForm and adding states

--- frontend/src/components/Expenses/Expenses.jsx
+++ frontend/src/components/Expenses/Expenses.jsx
@@ -1,44 +1,9 @@
 import React from "react";
-import classes from "./Expenses.module.css";
-import Button from "../Button/Button";
 
 function Expenses() {
-
-    const expenses = {
-        id: 1,
-        title: 'new computer',
-        price: 3000,
-        date: new Date('10-01-2024').toLocaleString(),
-    }
-
   return (
     <>
-    <div className={classes.boxForm}>
-    <form action="" className={classes.expensesForm}>
-      <div className={classes.expensesForm__expensesData}>
-        <div className={classes.expensesForm__expenseData}>
-          <label>Expense Title</label>
-          <input type="text" />
-        </div>
-        <div className={classes.expensesForm__expenseData}>
-          <label>Price</label>
-          <input type="number" name="" id="" />
-        </div>
-        <div className={classes.expensesForm__expenseData}>
-          <label>Date</label>
-          <input type="date" />
-        </div>
-      </div>
-      <Button type="submit" className={classes.expensesForm__btn}>Add expense</Button>
-    </form>
-    </div>
-
-   <div className={classes.section}>
-        <h2>Your Expenses</h2>
-        <ul>
-            <li id={expenses.id}><p>{expenses.title} {expenses.price}</p><span>{expenses.date}</span></li>
-        </ul>
-    </div> 
+ 
     </>
   );
 }
---
+++ frontend/src/components/Expenses/ExpensesForm.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import classes from "./Expenses.module.css";
+
+function Expenses() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredPrice, setEnteredPrice] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  function handleTitleChange(event) {
+    setEnteredTitle((title) => (title = event.target.value));
+  }
+
+  function handlePriceChange(event) {
+    setEnteredPrice((price) => (price = event.target.value));
+  }
+
+  function handleDateChange(event) {
+    setEnteredDate((date) => (date = event.target.value));
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={submitHandler} className={classes.expensesForm}>
+      <div className={classes.expensesForm__expensesData}>
+        <div className={classes.expensesForm__expenseData}>
+          <label>Expense Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={handleTitleChange}
+          />
+        </div>
+        <div className={classes.expensesForm__expenseData}>
+          <label>Price</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredPrice}
+            onChange={handlePriceChange}
+          />
+        </div>
+        <div className={classes.expensesForm__expenseData}>
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            value={enteredDate}
+            onChange={handleDateChange}
+          />
+        </div>
+      </div>
+      <button type="submit" className={classes.expensesForm__btn}>
+        Add expense
+      </button>
+    </form>
+  );
+}
+
+export default Expenses;
