c798a4daba196668aec81e35c5042677480bd989
rodis00 <happyekkov@gmail.com>
2024-01-21 23:05:28 +0100

    handle validation exceptions

--- backend/src/main/java/com/github/rodis00/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/github/rodis00/backend/exception/GlobalExceptionHandler.java
@@ -2,8 +2,13 @@ package com.github.rodis00.backend.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -29,5 +34,15 @@ public class GlobalExceptionHandler {
                 HttpStatus.NOT_FOUND,
                 e.getMessage()
         );
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, String> errorMap = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        });
+        return errorMap;
     }
 }
