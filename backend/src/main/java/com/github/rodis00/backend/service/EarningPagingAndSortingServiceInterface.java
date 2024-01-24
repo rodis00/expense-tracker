7722da90f7533a85842c52a08efddb6d31511386
rodis00 <happyekkov@gmail.com>
2024-01-24 20:34:51 +0100

    add paging and sorting service for earnings

---
+++ backend/src/main/java/com/github/rodis00/backend/service/EarningPagingAndSortingServiceInterface.java
@@ -0,0 +1,9 @@
+package com.github.rodis00.backend.service;
+
+import com.github.rodis00.backend.model.Earning;
+import com.github.rodis00.backend.model.GlobalPage;
+import org.springframework.data.domain.Page;
+
+public interface EarningPagingAndSortingServiceInterface {
+    Page<Earning> getEarningsByUserId(Integer userId, GlobalPage earningPage);
+}
