354e6f470d1476987ec88439a4a7dccb6cd99d91
rodis00 <happyekkov@gmail.com>
2024-01-25 17:55:52 +0100

    add paging and sorting expense repository

---
+++ backend/src/main/java/com/github/rodis00/backend/repository/ExpensePagingAndSortingRepository.java
@@ -0,0 +1,10 @@
+package com.github.rodis00.backend.repository;
+
+import com.github.rodis00.backend.model.Expense;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ExpensePagingAndSortingRepository extends PagingAndSortingRepository<Expense, Integer> {
+    Page<Expense> findByUserId(Integer userId, Pageable pageable);
+}
