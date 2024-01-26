ac442093324d6c3ec9cd8c24be66c1795cde1b4d
rodis00 <happyekkov@gmail.com>
2024-01-26 17:20:44 +0100

    change saved example data after api runs

--- backend/src/main/java/com/github/rodis00/backend/BackendApplication.java
+++ backend/src/main/java/com/github/rodis00/backend/BackendApplication.java
@@ -3,22 +3,30 @@ package com.github.rodis00.backend;
 import com.github.rodis00.backend.model.Earning;
 import com.github.rodis00.backend.model.Expense;
 import com.github.rodis00.backend.model.User;
+import com.github.rodis00.backend.repository.EarningRepository;
+import com.github.rodis00.backend.repository.ExpenseRepository;
 import com.github.rodis00.backend.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 
 @SpringBootApplication
 public class BackendApplication implements CommandLineRunner {
 	private final UserRepository userRepository;
+	private final ExpenseRepository expenseRepository;
+	private final EarningRepository earningRepository;
 
-	public BackendApplication(UserRepository userRepository) {
+	public BackendApplication(
+			UserRepository userRepository,
+			ExpenseRepository expenseRepository,
+			EarningRepository earningRepository
+	) {
 		this.userRepository = userRepository;
+		this.expenseRepository = expenseRepository;
+		this.earningRepository = earningRepository;
 	}
 
 	public static void main(String[] args) {
@@ -28,38 +36,41 @@ public class BackendApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		if (userRepository.findById(1).isEmpty()) {
-			Expense expense1 = new Expense();
-			expense1.setTitle("new computer");
-			expense1.setPrice(2500.99);
-			expense1.setDate(new Date());
-
-			Expense expense2 = new Expense();
-			expense2.setTitle("new car");
-			expense2.setPrice(12000.0);
-			expense2.setDate(new Date());
-
-			Earning earning1 = new Earning();
-			earning1.setTitle("bitcoin");
-			earning1.setAmount(5000.0);
-			earning1.setDate(new Date());
-
-			Earning earning2 = new Earning();
-			earning2.setTitle("house sale");
-			earning2.setAmount(28000.85);
-			earning2.setDate(new Date());
-
 			User user = new User();
 			user.setEmail("user@example.com");
 			user.setUsername("user");
 			user.setPassword("U$3r2024");
 			user.setExpenses(new ArrayList<>());
 			user.setEarnings(new ArrayList<>());
-
-			user.addExpense(expense1);
-			user.addExpense(expense2);
-			user.addEarning(earning1);
-			user.addEarning(earning2);
 			userRepository.save(user);
+
+			Expense expense1 = new Expense();
+			expense1.setTitle("new computer");
+			expense1.setPrice(2500.99);
+			expense1.setDate(new Date());
+			expense1.setUser(user);
+			expenseRepository.save(expense1);
+
+			Expense expense2 = new Expense();
+			expense2.setTitle("new car");
+			expense2.setPrice(12000.0);
+			expense2.setDate(new Date());
+			expense2.setUser(user);
+			expenseRepository.save(expense2);
+
+			Earning earning1 = new Earning();
+			earning1.setTitle("bitcoin");
+			earning1.setAmount(5000.0);
+			earning1.setDate(new Date());
+			earning1.setUser(user);
+			earningRepository.save(earning1);
+
+			Earning earning2 = new Earning();
+			earning2.setTitle("house sale");
+			earning2.setAmount(28000.85);
+			earning2.setDate(new Date());
+			earning2.setUser(user);
+			earningRepository.save(earning2);
 		}
 	}
 }
