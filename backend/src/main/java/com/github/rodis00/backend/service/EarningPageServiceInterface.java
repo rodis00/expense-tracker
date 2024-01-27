7e997d7cfb560172afb984b1c44ff8709691d1a0
rodis00 <happyekkov@gmail.com>
2024-01-27 11:46:43 +0100

    knowledge is the key to writing less and receiving more :)

--- backend/src/main/java/com/github/rodis00/backend/service/EarningPageServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.github.rodis00.backend.service;
-
-import com.github.rodis00.backend.model.Earning;
-import com.github.rodis00.backend.model.GlobalPage;
-import org.springframework.data.domain.Page;
-
-public interface EarningPageServiceInterface {
-    Page<Earning> getEarningsByUserId(Integer userId, GlobalPage earningPage);
-}
