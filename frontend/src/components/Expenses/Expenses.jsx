a7d1d4c4309b717997844badc17e7061b2a4c844
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-01-31 18:27:01 +0100

    new imports and renaming stuff contained in this component

--- frontend/src/components/Expenses/Expenses.jsx
+++ frontend/src/components/Expenses/Expenses.jsx
@@ -1,29 +1,30 @@
 import React, { useState } from "react";
-import AllExpenses from "./AllExpenses";
-import ExpensesForm from "./ExpensesForm";
+import UserData from "../ExpensesEarnings/UserData";
 import classes from "./Expenses.module.css";
+import UserForm from "../ExpensesEarnings/UserForm";
 
-const INITIAL_EXPENSES = [{
-  id: 'e1',
-  title: 'Doctor',
-  price: 200,
-  date: new Date(2021, 2, 28)
-}]
+const INITIAL_EXPENSES = [
+  {
+    id: "e1",
+    title: "Doctor",
+    price: 200,
+    date: new Date(2021, 2, 28),
+  },
+];
 
 function Expenses() {
-
   const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
 
-  function handleNewExpense(expense){
-    setExpenses((prevExpense)=>{
-      return[expense, ...prevExpense];
-    })
+  function handleNewExpense(expense) {
+    setExpenses((prevExpense) => {
+      return [expense, ...prevExpense];
+    });
   }
 
   return (
     <div className={classes.section}>
-      <ExpensesForm onSaveExpenseData={handleNewExpense}/>
-      <AllExpenses items={expenses} />
+      <UserForm onSaveExpenseData={handleNewExpense} />
+      <UserData items={expenses} />
     </div>
   );
 }
