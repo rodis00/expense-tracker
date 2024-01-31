a9d9f102ac5cb2fb00deeed2b3fd97bbb6f98568
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-01-31 18:03:10 +0100

    changing styles and coponents to display also adding dummy element to see how its displayed

--- frontend/src/components/Expenses/Expenses.jsx
+++ frontend/src/components/Expenses/Expenses.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
-import NewExpense from "./NewExpense";
+import AllExpenses from "./AllExpenses";
+import ExpensesForm from "./ExpensesForm";
+import classes from "./Expenses.module.css";
+
+const INITIAL_EXPENSES = [{
+  id: 'e1',
+  title: 'Doctor',
+  price: 200,
+  date: new Date(2021, 2, 28)
+}]
 
 function Expenses() {
-  
-  const [expenses, setExpenses] = useState();
+
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
 
   function handleNewExpense(expense){
     setExpenses((prevExpense)=>{
@@ -12,8 +21,9 @@ function Expenses() {
   }
 
   return (
-    <div>
-      <NewExpense onAddExpense={handleNewExpense}/>
+    <div className={classes.section}>
+      <ExpensesForm onSaveExpenseData={handleNewExpense}/>
+      <AllExpenses items={expenses} />
     </div>
   );
 }
