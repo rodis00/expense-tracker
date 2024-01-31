8d6868000195ca5b3cfbd8049cfc84b681b1326f
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-01-31 18:00:28 +0100

    item component that display one element that contain data about expenses

--- frontend/src/components/Expenses/ExpenseItem.jsx
+++ frontend/src/components/Expenses/ExpenseItem.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Date from '../Date/Date'
+import classes from './ExpenseItem.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-function ExpenseItem() {
+function ExpenseItem({title, price, date}) {
   return (
-    <li>
-    
+    <li className={classes.listItem}>
+      <Date date={date}/>
+      <h2 className={classes.listItem__title}>{title}</h2>
+      <div className={classes.listItem__price}><FontAwesomeIcon icon={faDollarSign}/><span>{price}</span></div>
+      <button className={classes.listItem__btn}><FontAwesomeIcon icon={faTrash} /></button>
     </li>
   )
 }
