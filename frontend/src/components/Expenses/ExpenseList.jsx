14d8b83db19452c4ec87d5360b997e6868193119
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-01-31 18:01:39 +0100

    list component that map all li with data added by user from form and adding styles

--- frontend/src/components/Expenses/ExpenseList.jsx
+++ frontend/src/components/Expenses/ExpenseList.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import ExpenseItem from './ExpenseItem';
+import classes from './ExpenseList.module.css'
 
-function ExpenseList() {
+function ExpenseList({items}) {
+
+  if(items.length === 0) {
+    return <h2 className={classes.list}>Found no expenses.</h2>
+  }
+
   return (
-    <ul>
-        
+    <ul className={classes.list}>
+        {items.map(prev => (
+          <ExpenseItem key={Math.random.toString()} title={prev.title} price={prev.price} date={prev.date}/>
+        ))}
     </ul>
   )
 }
