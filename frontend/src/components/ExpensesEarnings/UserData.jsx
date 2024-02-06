651528526ebb5e2f43629faeaffe70279c716ccb
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-02-06 10:39:37 +0100

    displaying info about total amount of expenses and adding styles

--- frontend/src/components/ExpensesEarnings/UserData.jsx
+++ frontend/src/components/ExpensesEarnings/UserData.jsx
@@ -2,11 +2,28 @@ import React from "react";
 import classes from "./UserData.module.css";
 import ListData from "./ListData";
 import Chart from "../Chart/Chart";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
 
 function UserData({ items }) {
+  let amount = 0;
+
+  for (const element of items) {
+    amount += element.price;
+  }
+
   return (
     <>
-      <h2 className={classes.total}>Total expenses: </h2>
+      <h2 className={classes.total}>
+        Total expenses:
+        {amount <= 0 ? (
+          ""
+        ) : (
+          <span>
+            -{amount} <FontAwesomeIcon icon={faDollarSign} />
+          </span>
+        )}
+      </h2>
       <Chart items={items} />
       <div className={classes.section}>
         <ListData items={items} />
