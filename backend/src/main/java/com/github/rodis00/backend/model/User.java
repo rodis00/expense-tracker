e1ade9951f79168783454a21a9089c5059783f2c
rodis00 <happyekkov@gmail.com>
2024-02-18 19:24:08 +0100

    implement user details

--- backend/src/main/java/com/github/rodis00/backend/model/User.java
+++ backend/src/main/java/com/github/rodis00/backend/model/User.java
@@ -8,13 +8,16 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Data
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonIgnore
@@ -55,4 +58,29 @@ public class User {
     @JsonManagedReference
     @JsonIgnore
     private List<Expense> expenses;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
