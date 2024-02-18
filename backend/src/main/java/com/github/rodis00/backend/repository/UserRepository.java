43e6ef467477a44186af32768c93aa968a358bf5
rodis00 <happyekkov@gmail.com>
2024-02-18 20:41:48 +0100

    add method findByUsername

--- backend/src/main/java/com/github/rodis00/backend/repository/UserRepository.java
+++ backend/src/main/java/com/github/rodis00/backend/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.github.rodis00.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsByEmail(String email);
@@ -11,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsByUsername(String username);
 
     User findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
 }
