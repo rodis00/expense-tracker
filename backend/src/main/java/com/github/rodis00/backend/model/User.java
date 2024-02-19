4f72ab0ac39f69ca61c2ff595fd4cd8a99b85398
rodis00 <happyekkov@gmail.com>
2024-02-19 10:04:24 +0100

    delete unnecessary annotations in the User model

--- backend/src/main/java/com/github/rodis00/backend/model/User.java
+++ backend/src/main/java/com/github/rodis00/backend/model/User.java
@@ -2,11 +2,7 @@ package com.github.rodis00.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.github.rodis00.backend.utils.customValidator.Password;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,22 +19,16 @@ public class User implements UserDetails {
     @JsonIgnore
     private Integer id;
 
-    @NotBlank(message = "Username shouldn't be empty.")
-    @Size(min = 3, max = 20, message = "Username should be between 3 or 20 characters.")
     @Column(
             unique = true
     )
     private String username;
 
-    @NotBlank(message = "Email shouldn't be null.")
-    @Email(message = "Invalid email address.")
     @Column(
             unique = true
     )
     private String email;
 
-    @NotBlank(message = "Password shouldn't be null.")
-    @Password
     private String password;
 
     @OneToMany(
