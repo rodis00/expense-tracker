1a231374457200828a77fe7abba7b835587377a0
rodis00 <happyekkov@gmail.com>
2024-02-19 09:55:05 +0100

    add authenticate service

---
+++ backend/src/main/java/com/github/rodis00/backend/auth/AuthenticateService.java
@@ -0,0 +1,75 @@
+package com.github.rodis00.backend.auth;
+
+import com.github.rodis00.backend.config.JwtService;
+import com.github.rodis00.backend.exception.InvalidPasswordException;
+import com.github.rodis00.backend.exception.UserAlreadyExistsException;
+import com.github.rodis00.backend.exception.UserNotFoundException;
+import com.github.rodis00.backend.exception.UsernameIsTakenException;
+import com.github.rodis00.backend.model.User;
+import com.github.rodis00.backend.repository.UserRepository;
+import com.github.rodis00.backend.service.UserService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticateService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+
+    public AuthenticateService(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            JwtService jwtService,
+            AuthenticationManager authenticationManager, UserService userService
+    ) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+        this.userService = userService;
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        if (userService.existsByUsername(request.getUsername()))
+            throw new UsernameIsTakenException("This username is taken.");
+
+        if (userService.existsByEmail(request.getEmail()))
+            throw new UserAlreadyExistsException("User with this email already exists.");
+
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setUsername(request.getUsername());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        userRepository.save(user);
+
+        String jwtToken = jwtService.generateToken(user);
+
+        return new AuthResponse(jwtToken);
+    }
+
+    public AuthResponse authenticate(AuthRequest request) {
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new UserNotFoundException("User not found."));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword()))
+            throw new InvalidPasswordException("Invalid password.");
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        String jwtToken = jwtService.generateToken(user);
+
+        return new AuthResponse(jwtToken);
+    }
+}
