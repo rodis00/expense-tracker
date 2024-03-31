8c2c8a7c7165010534a8354a67372c0c5b3a59a4
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-03-31 18:49:11 +0200

    importing new things, adding modal, position staff better with gathering options in selectors

--- frontend/src/components/ExpensesEarnings/UserData.jsx
+++ frontend/src/components/ExpensesEarnings/UserData.jsx
@@ -4,10 +4,22 @@ import ListData from "./ListData";
 import Chart from "../Chart/Chart";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import YearFilter from "./YearFilter";
+import { useDispatch, useSelector } from "react-redux";
+import { modalActions } from "../../store/modal-slice";
+import Modal from "../Modal/Modal";
+import UserForm from "./UserForm";
 
-function UserData({ items, name }) {
+function UserData({ items, name, secondName, upperName, amountName }) {
   const [selectedYear, setSelectedYear] = useState("2024");
+
+  const version = useSelector((state) => state.modal.modalVersion);
+  const dispatch = useDispatch();
+
+  function handleOpenModalForm() {
+    dispatch(modalActions.showForm());
+  }
 
   const handleFilteredYear = (year) => {
     setSelectedYear(year);
@@ -25,26 +37,50 @@ function UserData({ items, name }) {
 
   return (
     <>
-      <h2 className={classes.total}>
-        Total {name}:
-        {amount <= 0 ? (
-          ""
-        ) : (
-          <span
-            className={`${
-              name === "expenses" ? classes.total__span : classes.total__span2
-            }`}
+      <Modal open={version}>
+        <UserForm
+          // onSaveUserData={handleNewExpense}
+          name={secondName}
+          secondName={upperName}
+          amount={amountName}
+        />
+      </Modal>
+      <main className={classes.main}>
+        <h2 className={classes.total}>
+          Total {name}:
+          {amount <= 0 ? (
+            ""
+          ) : (
+            <span
+              className={`${
+                name === "expenses" ? classes.total__span : classes.total__span2
+              }`}
+            >
+              {name === "expenses" ? "-" : ""}
+              {amount} <FontAwesomeIcon icon={faDollarSign} />
+            </span>
+          )}
+        </h2>
+        <Chart items={filteredItems} name={name} />
+        <div className={classes.options}>
+          <button
+            className={classes.modalOpenBtn}
+            onClick={handleOpenModalForm}
           >
-            {name === "expenses" ? "-" : ""}
-            {amount} <FontAwesomeIcon icon={faDollarSign} />
-          </span>
-        )}
-      </h2>
-      <Chart items={filteredItems} />
-      <YearFilter selected={selectedYear} onYearChange={handleFilteredYear} />
-      <div className={classes.section}>
-        <ListData items={filteredItems} name={name} />
-      </div>
+            Add new {secondName}
+            <span>
+              <FontAwesomeIcon icon={faPlus} />
+            </span>
+          </button>
+          <YearFilter
+            selected={selectedYear}
+            onYearChange={handleFilteredYear}
+          />
+        </div>
+        <div className={classes.section}>
+          <ListData items={filteredItems} name={name} />
+        </div>
+      </main>
     </>
   );
 }
