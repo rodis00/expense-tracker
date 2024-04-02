c7d07fd7f81201140ac3eb4250864ec6dc85352e
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-04-02 11:27:12 +0200

    adding routes to app with createBrowserRouter that was previously declared in nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/Nav/Nav";
+import RootLayout from "./components/RootLayout";
+import Home from "./components/Home/Home";
+import Expenses from "./components/Expenses/Expenses";
+import Earnings from "./components/Earnings/Earnings";
+import Summaries from "./components/Summaries/Summaries";
+import Login from "./components/Login/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    id: "root",
+    children: [
+      { index: true, element: <Home /> },
+      { path: "expenses", element: <Expenses /> },
+      { path: "earnings", element: <Earnings /> },
+      { path: "summaries", element: <Summaries /> },
+      { path: "login", element: <Login /> },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <Nav />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
