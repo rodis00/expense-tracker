123b3c18b0ddf325112480a33c25a353dd2034a3
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-04-15 10:55:12 +0200

    adding authorization

--- frontend/src/components/RootLayout.jsx
+++ frontend/src/components/RootLayout.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import Nav from "./Nav/Nav";
 import { Outlet } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/auth-slice";
+import { jwtDecode } from "jwt-decode";
 
 function RootLayout() {
+  const dispatch = useDispatch();
+
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    const userId = jwtDecode(token).userId;
+
+    dispatch(authActions.login(userId));
+  }
+
   return (
     <>
       <Nav />
