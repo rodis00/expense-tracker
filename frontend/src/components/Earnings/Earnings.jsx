2c7548ae3ee0ccfa73bac048b8002552c3aa50e7
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-04-16 11:17:52 +0200

    adding option to fetch earnings from database with useCallback to not rerender multiple times and creating new array to add modified data

--- frontend/src/components/Earnings/Earnings.jsx
+++ frontend/src/components/Earnings/Earnings.jsx
@@ -1,18 +1,55 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import UserData from "../ExpensesEarnings/UserData";
 import classes from "./Earnings.module.css";
+import { useSelector } from "react-redux";
 
-const INITIAL_EARNINGS = [
-  {
-    id: "e1",
-    title: "Work",
-    amount: 200,
-    date: new Date(2021, 2, 28),
-  },
-];
+const EARNINGS = [];
 
 function Earnings() {
-  const [earnings, setEarnings] = useState(INITIAL_EARNINGS);
+  const [earnings, setEarnings] = useState([]);
+
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const user = useSelector((state) => state.auth.user);
+
+  const token = localStorage.getItem("token");
+
+  const fetchExpenses = useCallback(async () => {
+    if (isAuthenticated) {
+      const response = await fetch(
+        `http://localhost:8080/expense-tracker/api/v1/earnings/pages/users/${user}`,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+
+      const resData = await response.json();
+
+      resData.content.map((item) => {
+        let date = new Date(item.date);
+        const newEarning = {
+          id: item.id,
+          title: item.title,
+          amount: +item.amount,
+          date,
+        };
+
+        const isExisting = EARNINGS.some((item) => {
+          return item.id === newEarning.id;
+        });
+
+        if (!isExisting) EARNINGS.push(newEarning);
+
+        return EARNINGS;
+      });
+      setEarnings(EARNINGS);
+    }
+  }, [isAuthenticated, user, token]);
+
+  useEffect(() => {
+    fetchExpenses();
+  }, [fetchExpenses]);
 
   // function handleNewEarning(earning) {
   //   setEarnings((prevEarning) => {
