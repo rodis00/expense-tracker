6757dfb7fe2dba2a5c66d91f67bab06756b874b9
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-04-16 11:17:39 +0200

    adding option to fetch expenses from database with useCallback to not rerender multiple times and creating new array to add modified data

--- frontend/src/components/Expenses/Expenses.jsx
+++ frontend/src/components/Expenses/Expenses.jsx
@@ -1,25 +1,61 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import UserData from "../ExpensesEarnings/UserData";
 import classes from "./Expenses.module.css";
-import UserForm from "../ExpensesEarnings/UserForm";
+import { useSelector } from "react-redux";
 
-const INITIAL_EXPENSES = [
-  {
-    id: "e1",
-    title: "Doctor",
-    amount: 200,
-    date: new Date(2021, 2, 28),
-  },
-];
+const EXPENSES = [];
 
 function Expenses() {
-  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
+  const [expenses, setExpenses] = useState([]);
 
-  function handleNewExpense(expense) {
-    setExpenses((prevExpense) => {
-      return [expense, ...prevExpense];
-    });
-  }
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const user = useSelector((state) => state.auth.user);
+
+  const token = localStorage.getItem("token");
+
+  const fetchExpenses = useCallback(async () => {
+    if (isAuthenticated) {
+      const response = await fetch(
+        `http://localhost:8080/expense-tracker/api/v1/expenses/pages/users/${user}`,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+
+      const resData = await response.json();
+
+      resData.content.map((item) => {
+        let date = new Date(item.date);
+        const newExpense = {
+          id: item.id,
+          title: item.title,
+          amount: +item.price,
+          date,
+        };
+
+        const isExisting = EXPENSES.some((item) => {
+          return item.id === newExpense.id;
+        });
+
+        if (!isExisting) EXPENSES.push(newExpense);
+
+        return EXPENSES;
+      });
+      setExpenses(EXPENSES);
+    }
+  }, [isAuthenticated, user, token]);
+
+  useEffect(() => {
+    fetchExpenses();
+  }, [fetchExpenses]);
+
+  // function handleNewExpense(expense) {
+  //   setExpenses((prevExpense) => {
+  //     return [expense, ...prevExpense];
+  //   });
+  // }
 
   return (
     <div className={classes.section}>
