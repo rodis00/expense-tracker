46900a248f69d5dece17120b5042cca48e6a4642
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-04-16 12:49:44 +0200

    adding new option to add expense and earnings to database

--- frontend/src/components/ExpensesEarnings/UserForm.jsx
+++ frontend/src/components/ExpensesEarnings/UserForm.jsx
@@ -1,78 +1,73 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import classes from "./UserForm.module.css";
+import { json } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { useDispatch, useSelector } from "react-redux";
 import { modalActions } from "../../store/modal-slice";
 
-function UserForm({ onSaveUserData, name, secondName, amount }) {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredAmount, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
-
+function UserForm({ httpName, httpAmount, name, secondName, amount }) {
   const dateInputRef = useRef();
 
   const dispatch = useDispatch();
 
   const version = useSelector((state) => state.modal.modalVersion);
 
+  const user = useSelector((state) => state.auth.user);
+
+  const token = localStorage.getItem("token");
+
   function handleCloseForm() {
     dispatch(modalActions.closeModal());
-  }
-
-  function handleTitleChange(event) {
-    setEnteredTitle((title) => (title = event.target.value));
-  }
-
-  function handleAmountChange(event) {
-    setEnteredAmount((price) => (price = event.target.value));
-  }
-
-  function handleDateChange(event) {
-    setEnteredDate((date) => (date = event.target.value));
   }
 
   function handleFocus() {
     dateInputRef.current.showPicker();
   }
 
-  function submitHandler(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
 
     const userData = {
-      title: enteredTitle,
-      amount: +enteredAmount,
-      date: new Date(enteredDate),
+      title: data.title,
+      [httpAmount]: data.amount,
+      date: data.date.toString(),
     };
 
-    onSaveUserData(userData);
-    setEnteredTitle("");
-    setEnteredAmount("");
-    setEnteredDate("");
+    const response = await fetch(
+      `http://localhost:8080/expense-tracker/api/v1/${httpName}/users/${user}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+        body: JSON.stringify(userData),
+      }
+    );
+
+    if (!response.ok) {
+      throw json({ message: "Could not add data." }, { status: 500 });
+    }
   }
 
   return (
-    <form onSubmit={submitHandler} className={classes.userForm}>
+    <form onSubmit={handleSubmit} className={classes.userForm}>
       <div className={classes.userForm__itemsData}>
         <div className={classes.userForm__itemData}>
           <label>{secondName} Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={handleTitleChange}
-            placeholder="Enter Title"
-            required
-          />
+          <input type="text" name="title" placeholder="Enter Title" required />
         </div>
         <div className={classes.userForm__itemData}>
           <label>{amount}</label>
           <input
             type="number"
+            name="amount"
             min="0.01"
             step="0.01"
             placeholder={`Enter ${amount}`}
-            value={enteredAmount}
-            onChange={handleAmountChange}
             required
           />
         </div>
@@ -80,9 +75,8 @@ function UserForm({ onSaveUserData, name, secondName, amount }) {
           <label>Date</label>
           <input
             type="date"
+            name="date"
             min="2021-01-01"
-            value={enteredDate}
-            onChange={handleDateChange}
             onFocus={handleFocus}
             ref={dateInputRef}
             required
@@ -93,6 +87,7 @@ function UserForm({ onSaveUserData, name, secondName, amount }) {
         <button
           className={classes.userForm__closeBtn}
           onClick={handleCloseForm}
+          type="button"
         >
           Close
         </button>
