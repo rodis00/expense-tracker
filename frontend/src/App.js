70dfb928196770623df08b3a35c517d8fb5a0f3a
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-05-01 19:23:42 +0200

    importing protected routes and error page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,17 +7,25 @@ import Earnings from "./components/Earnings/Earnings";
 import Summaries from "./components/Summaries/Summaries";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
+import Error from "./components/Error-page/Error";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <Error />,
     id: "root",
     children: [
       { index: true, element: <Home /> },
-      { path: "expenses", element: <Expenses /> },
-      { path: "earnings", element: <Earnings /> },
-      { path: "summaries", element: <Summaries /> },
+      {
+        element: <ProtectedRoute />,
+        children: [
+          { path: "expenses", element: <Expenses /> },
+          { path: "earnings", element: <Earnings /> },
+          { path: "summaries", element: <Summaries /> },
+        ],
+      },
       { path: "login", element: <Login /> },
       { path: "signup", element: <Signup /> },
     ],
