03ed82cad21bc4ec8a3eea0495aee3880d3b795a
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-05-13 10:23:51 +0200

    import fetch functions and pass data to summaries content component

--- frontend/src/components/Summaries/Summaries.jsx
+++ frontend/src/components/Summaries/Summaries.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./Summaries.module.css";
-import ChartLine from "../Chart/ChartLine";
+import { fetchAllEarnings, fetchAllExpenses } from "./FetchExpensesEarnings";
+import { useDispatch, useSelector } from "react-redux";
+import SummariesContent from "./SummariesContent";
 
 function Summaries() {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const user = useSelector((state) => state.auth.user);
+
+  const token = localStorage.getItem("token");
+
+  const allExpenses = useSelector((state) => state.allExpenses.items);
+
+  const allEarnings = useSelector((state) => state.allEarnings.items);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      fetchAllExpenses(user, token, dispatch);
+      fetchAllEarnings(user, token, dispatch);
+    }
+  }, [user, token, isAuthenticated, dispatch]);
+
   return (
     <div className={classes.section}>
       <div>
         <h2 className={classes.title}>Your Summaries</h2>
-        <div>
-          <ChartLine />
-        </div>
+        <SummariesContent allExpenses={allExpenses} allEarnings={allEarnings} />
       </div>
     </div>
   );
