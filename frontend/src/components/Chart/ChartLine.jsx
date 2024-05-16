4427b7e899687fe372b45b74fa28cc7f1cccd70f
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-05-16 11:05:13 +0200

    change styles for chart grid and labels

--- frontend/src/components/Chart/ChartLine.jsx
+++ frontend/src/components/Chart/ChartLine.jsx
@@ -71,10 +71,41 @@ function ChartLine({ expenses, earnings }) {
               ticks: {
                 color: "white",
               },
+              grid: {
+                display: false,
+              },
+              title: {
+                display: true,
+                text: "Months",
+                color: "rgb(100,100,100)",
+                font: {
+                  size: "20px",
+                },
+              },
             },
             y: {
               ticks: {
                 color: "white",
+              },
+              grid: {
+                color: "rgb(35,35,35)",
+              },
+              title: {
+                display: true,
+                text: "Currency - $",
+                color: "rgb(100,100,100)",
+                font: {
+                  size: "20px",
+                },
+              },
+            },
+          },
+          plugins: {
+            legend: {
+              labels: {
+                font: {
+                  size: "17px",
+                },
               },
             },
           },
