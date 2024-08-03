b492d5697b4c7c7e564b049faeed0f317e288958
rodis00 <happyekkov@gmail.com>
2024-08-03 19:19:22 +0200

    remove user service interface

--- backend/src/main/java/com/github/rodis00/backend/user/UserService.java
+++ backend/src/main/java/com/github/rodis00/backend/user/UserService.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Service
-public class UserService implements UserServiceInterface {
+public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -23,14 +23,12 @@ public class UserService implements UserServiceInterface {
         this.passwordEncoder = passwordEncoder;
     }
 
-    @Override
     public User getUserById(Integer id) {
         return userRepository
                 .findById(id)
                 .orElseThrow(() -> new UserNotFoundException("User not found."));
     }
 
-    @Override
     public UserDto updateUser(
             Integer id,
             UserRequest user
@@ -56,23 +54,19 @@ public class UserService implements UserServiceInterface {
         return UserDto.from(existingUser);
     }
 
-    @Override
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-    @Override
     public void deleteUserById(Integer id) {
         User user = getUserById(id);
         userRepository.delete(user);
     }
 
-    @Override
     public boolean existsByEmail(String email) {
         return userRepository.existsByEmail(email);
     }
 
-    @Override
     public boolean existsByUsername(String username) {
         return userRepository.existsByUsername(username);
     }
