519fb9132d1c03af1676d849d93d06d51fa176e1
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-08-09 12:34:29 +0200

    recreate summaries component

--- frontend/src/components/Summaries/Summaries.jsx
+++ frontend/src/components/Summaries/Summaries.jsx
@@ -1,35 +1,113 @@
-import React, { useEffect } from "react";
-import classes from "./Summaries.module.css";
-import { fetchAllEarnings, fetchAllExpenses } from "./FetchExpensesEarnings";
-import { useDispatch, useSelector } from "react-redux";
-import SummariesContent from "./SummariesContent";
+import React from "react";
+import LineChart from "../charts/LineChart";
+import DoughnutChart from "../charts/DoughnutChart";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
-function Summaries() {
-  const dispatch = useDispatch();
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-  const user = useSelector((state) => state.auth.user);
-
-  const token = localStorage.getItem("token");
-
-  const allExpenses = useSelector((state) => state.allExpenses.items);
-
-  const allEarnings = useSelector((state) => state.allEarnings.items);
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      fetchAllExpenses(user, token, dispatch);
-      fetchAllEarnings(user, token, dispatch);
-    }
-  }, [user, token, isAuthenticated, dispatch]);
-
+const Summaries = () => {
   return (
-    <div className={classes.section}>
-      <div>
-        <h2 className={classes.title}>Your Summaries</h2>
-        <SummariesContent allExpenses={allExpenses} allEarnings={allEarnings} />
+    <main className="w-full min-h-screen flex flex-col items-center text-white overflow-x-hidden">
+      <div className="text-center">
+        <h1 className="text-xl mt-4 text-neutral-600">Total Balance</h1>
+        <span className="text-3xl text-amber-300">$10000</span>
       </div>
-    </div>
+      <div className="w-full md:w-3/4 lg:w-full flex justify-center">
+        <LineChart name="summaries" />
+      </div>
+      <div className="w-[80%] flex flex-col lg:flex-row mt-6 gap-12">
+        <section className="w-full lg:w-[55%] xlg:w-1/2 flex flex-col items-center gap-8 lg:mb-8">
+          <div className="h-1/2 px-4">
+            <div className="flex flex-col items-center">
+              <h2 className="text-amber-300 text-2xl rounded-full font-semibold my-8 bg-thirdColor shadow-lg shadow-neutral-800 w-80 h-12 flex justify-center items-center">
+                Saving Plans
+              </h2>
+              <button className=" bg-secondColor w-40 h-12 rounded-full transition-all duration-300 hover:bg-[#28bf8a]">
+                Add New Plan
+              </button>
+              <ul className="w-full flex items-center mt-8 gap-4 lg:gap-3 xlg:gap-4">
+                <li className="bg-fourthColor h-24 w-40 rounded-3xl transition-all duration-300 cursor-pointer hover:bg-neutral-800">
+                  <h3 className="text-neutral-600 pl-4 pt-2">House</h3>
+                  <p className="pl-4 text-xl font-semibold">200000$</p>
+                  <progress
+                    value={20000}
+                    max={200000}
+                    className="h-2 w-[80%] [&::-webkit-progress-bar]:rounded-3xl [&::-webkit-progress-value]:rounded-3xl [&::-webkit-progress-bar]:bg-neutral-700 [&::-webkit-progress-value]:bg-[#28bf8a] block mx-auto mt-4"
+                  />
+                </li>
+                <li className="bg-fourthColor h-24 w-40 rounded-3xl transition-all duration-300 cursor-pointer hover:bg-neutral-800">
+                  <h3 className="text-neutral-600 pl-4 pt-2">Car</h3>
+                  <p className="pl-4 text-xl font-semibold">20000$</p>
+                  <progress
+                    value={10000}
+                    max={20000}
+                    className="h-2 w-[80%] [&::-webkit-progress-bar]:rounded-3xl [&::-webkit-progress-value]:rounded-3xl [&::-webkit-progress-bar]:bg-neutral-700 [&::-webkit-progress-value]:bg-[#28bf8a] block mx-auto mt-4"
+                  />
+                </li>
+                <li className="bg-fourthColor h-24 w-40 rounded-3xl transition-all duration-300 cursor-pointer hover:bg-neutral-800">
+                  <h3 className="text-neutral-600 pl-4 pt-2">Books</h3>
+                  <p className="pl-4 text-xl font-semibold">1000$</p>
+                  <progress
+                    value={550}
+                    max={1000}
+                    className="h-2 w-[80%] [&::-webkit-progress-bar]:rounded-3xl [&::-webkit-progress-value]:rounded-3xl [&::-webkit-progress-bar]:bg-neutral-700 [&::-webkit-progress-value]:bg-[#28bf8a] block mx-auto mt-4"
+                  />
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="h-1/2 px-4">
+            <div className="flex flex-col items-center">
+              <h2 className="text-amber-300 text-2xl rounded-full font-semibold my-8 bg-thirdColor shadow-lg shadow-neutral-800 w-80 h-12 flex justify-center items-center">
+                Schedules
+              </h2>
+              <button className=" bg-secondColor w-40 h-12 rounded-full transition-all duration-300 hover:bg-[#28bf8a]">
+                Add New Schedule
+              </button>
+              <ul className="w-full flex items-center mt-8 gap-4 lg:gap-3 xlg:gap-4">
+                <li className="h-24 w-40 bg-fourthColor rounded-3xl transition-all duration-300 cursor-pointer hover:bg-neutral-800">
+                  <h3 className="text-neutral-600 pl-4 pt-2">Rent</h3>
+                  <p className="pl-4 text-xl font-semibold">200$</p>
+                  <p className="pl-4 text-sm text-amber-300">
+                    Once - 28.09.2024
+                  </p>
+                </li>
+                <li className="h-24 w-40 bg-fourthColor rounded-3xl transition-all duration-300 cursor-pointer hover:bg-neutral-800">
+                  <h3 className="text-neutral-600 pl-4 pt-2">Netflix</h3>
+                  <p className="pl-4 text-xl font-semibold">20$</p>
+                  <p className="pl-4 text-sm text-amber-300">
+                    Monthly - 28.09.2024
+                  </p>
+                </li>
+                <li className="h-24 w-40 bg-fourthColor rounded-3xl transition-all duration-300 cursor-pointer hover:bg-neutral-800">
+                  <h3 className="text-neutral-600 pl-4 pt-2">Car insurance</h3>
+                  <p className="pl-4 text-xl font-semibold">500$</p>
+                  <p className="pl-4 text-sm text-amber-300">
+                    Yearly - 28.09.2024
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+
+        <section className="w-full lg:w-[45%] xlg:w-1/2 flex flex-col items-center gap-2 lg:gap-8 mb-4 lg:mb-8">
+          <div className="w-[90%] sm:w-3/4 md:w-3/5 lg:w-[80%]">
+            <h2 className="text-2xl font-semibold text-secondColor text-left my-8">
+              Incomes <span className="text-neutral-700 text-base pl-4">(by category)</span>
+            </h2>
+            <DoughnutChart />
+          </div>
+          <div className="w-[90%] sm:w-3/4 md:w-3/5 lg:w-[80%]">
+            <h2 className="text-2xl font-semibold text-red-500 text-left my-8">
+              Expenses <span className="text-neutral-700 text-base pl-4">(by category)</span>
+            </h2>
+            <DoughnutChart />
+          </div>
+        </section>
+      </div>
+    </main>
   );
-}
+};
 
 export default Summaries;
