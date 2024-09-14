8ab0c0ba027c7e2442dc34607c79909d17eb854f
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-09-14 22:08:53 +0200

    add api connection for fetching expenses and incomes

--- frontend/src/util/http/incomeHttp.jsx
+++ frontend/src/util/http/incomeHttp.jsx
@@ -20,3 +20,30 @@ export async function newIncome({ userId, values, token }) {
 
   return result;
 }
+
+export async function fetchIncomes({
+  userId,
+  token,
+  pageSize,
+  year,
+  pageNumber,
+}) {
+  const response = await fetch(
+    `http://localhost:8080/expense-tracker/api/v1/incomes/pages/users/${userId}?pageSize=${pageSize}&year=${year}&pageNumber=${pageNumber}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    const error = await response.json();
+    throw error.message;
+  }
+
+  const result = await response.json();
+
+  return result;
+}
