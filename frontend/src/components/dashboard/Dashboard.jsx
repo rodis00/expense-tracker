f12ad49ac19f182151fedd981da9630900b7bb42
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-10-01 10:28:52 +0200

    add fetching data to fill component with values from api

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -4,17 +4,63 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faArrowTrendUp,
   faArrowTrendDown,
+  faDollar,
   faBriefcase,
 } from "@fortawesome/free-solid-svg-icons";
 import { DUMMY_DATA_EXPENSES } from "../../DummyData";
-import DoughnutChart from "../charts/DoughnutChart";
+import DashboardValueListElement from "./DashboardValueListElement";
+import { useQuery } from "@tanstack/react-query";
+import { useSelector } from "react-redux";
+import { fetchAllIncomes } from "../../util/http/incomeHttp";
+import { fetchAllExpenses } from "../../util/http/expenseHttp";
 
 const Dashboard = () => {
   const testArray = DUMMY_DATA_EXPENSES.slice(0, 3);
+  const token = localStorage.getItem("token");
+  const userId = useSelector((state) => state.auth.user);
+  let incomes = 0;
+  let expenses = 0;
+  let balance = 0;
+  let minIncome = 0;
+  let maxIncome = 0;
+  let minExpense = 0;
+  let maxExpense = 0;
 
-  let incomes = 80000;
-  let expenses = 60000;
-  let balance = incomes - expenses;
+  const { data: incomesData, isPending: incomesPending } = useQuery({
+    queryKey: ["incomes", { userId, token }],
+    queryFn: () => fetchAllIncomes({ userId, token }),
+    enabled: !!userId,
+  });
+
+  const { data: expensesData, isPending: expensePending } = useQuery({
+    queryKey: ["expenses", { userId, token }],
+    queryFn: () => fetchAllExpenses({ userId, token }),
+    enabled: !!userId,
+  });
+
+  if (incomesData) {
+    incomesData.forEach((item) => (incomes += item.amount));
+    minIncome = incomesData.reduce((item, current) => {
+      return current.amount < item ? current.amount : item;
+    }, incomesData[0].amount);
+    maxIncome = incomesData.reduce((item, current) => {
+      return current.amount > item ? current.amount : item;
+    }, incomesData[0].amount);
+  }
+
+  if (expensesData) {
+    expensesData.forEach((item) => (expenses += item.price));
+    minExpense = expensesData.reduce((item, current) => {
+      return current.price < item ? current.price : item;
+    }, expensesData[0].price);
+    maxExpense = expensesData.reduce((item, current) => {
+      return current.price > item ? current.price : item;
+    }, expensesData[0].price);
+  }
+
+  if (expensesData && incomesData) {
+    balance = incomes - expenses;
+  }
 
   return (
     <main className="w-full min-h-screen flex flex-col text-white">
@@ -25,51 +71,35 @@ const Dashboard = () => {
       <div className="flex flex-col items-center flex-grow mt-12 lg:items-start lg:flex-row lg:ml-12">
         <section className="w-full md:w-3/4 lg:w-[70%] h-full flex flex-col items-center justify-center">
           <ul className="w-full lg:w-[85%] flex flex-col xsm:flex-row justify-center items-center md:gap-8 ">
-            <li className="w-[90%] xsm:w-1/3 lg:w-1/3 xsm:h-36 h-20 xlg:h-24 bg-thirdColor shadow-lg shadow-neutral-0 lg:shadow-neutral-800 mt-4 xsm:mt-0">
-              <div className="w-full h-full flex xsm:flex-col xlg:flex-row items-center">
-                <div className="w-14 h-14 mx-4 border-2 border-yellow-500 flex justify-center items-center xsm:mt-4 xlg:mt-0">
-                  <FontAwesomeIcon icon={faArrowTrendUp} className="text-2xl" />
-                </div>
-                <div className="grow xsm:text-center xlg:text-left">
-                  <h2 className="text-neutral-600 text-lg xlg:text-xl mt-2 xlg:mt-0">
-                    Available Balance
-                  </h2>
-                  <span className="text-xl xlg:text-2xl">{balance}</span>
-                </div>
-              </div>
-            </li>
-            <li className="w-[90%] xsm:w-1/3 lg:w-1/3 xsm:h-36 h-20 xlg:h-24 bg-thirdColor shadow-lg shadow-neutral-0 lg:shadow-neutral-800 mt-4 xsm:mt-0">
-              <div className="w-full h-full flex xsm:flex-col xlg:flex-row items-center">
-                <div className="w-14 h-14 mx-4 border-2 border-secondColor flex justify-center items-center xsm:mt-4 xlg:mt-0">
-                  <FontAwesomeIcon icon={faArrowTrendUp} className="text-2xl" />
-                </div>
-                <div className="grow xsm:text-center xlg:text-left">
-                  <h2 className="text-neutral-600 text-lg xlg:text-xl mt-2 xlg:mt-0">
-                    Incomes
-                  </h2>
-                  <span className="text-xl xlg:text-2xl">{incomes}</span>
-                </div>
-              </div>
-            </li>
-            <li className="w-[90%] xsm:w-1/3 lg:w-1/3 xsm:h-36 h-20 xlg:h-24 bg-thirdColor shadow-lg shadow-neutral-0 lg:shadow-neutral-800 mt-4 xsm:mt-0">
-              <div className="w-full h-full flex xsm:flex-col xlg:flex-row items-center">
-                <div className="w-14 h-14 mx-4 border-2 border-red-400 flex justify-center items-center xsm:mt-4 xlg:mt-0">
-                  <FontAwesomeIcon
-                    icon={faArrowTrendDown}
-                    className="text-2xl"
-                  />
-                </div>
-                <div className="grow xsm:text-center xlg:text-left">
-                  <h2 className="text-neutral-600 text-lg xlg:text-xl mt-2 xlg:mt-0">
-                    Expenses
-                  </h2>
-                  <span className="text-xl xlg:text-2xl">{expenses}</span>
-                </div>
-              </div>
-            </li>
+            <DashboardValueListElement
+              border="border-yellow-500"
+              color="text-yellow-500"
+              title="Available balance"
+              value={balance}
+              icon={faDollar}
+            />
+            <DashboardValueListElement
+              border="border-secondColor"
+              color="text-secondColor"
+              title="Incomes"
+              value={incomes}
+              icon={faArrowTrendUp}
+            />
+            <DashboardValueListElement
+              border="border-red-400"
+              color="text-red-400"
+              title="Expenses"
+              value={expenses}
+              icon={faArrowTrendDown}
+            />
           </ul>
           <div className="w-full mt-8 flex justify-center">
-            <LineChart />
+            <LineChart
+              incomesData={incomesData}
+              expensesData={expensesData}
+              incomesPending={incomesPending}
+              expensePending={expensePending}
+            />
           </div>
         </section>
 
@@ -94,12 +124,12 @@ const Dashboard = () => {
           </ul>
           <div className="flex justify-between mt-12 w-[95%]">
             <p className="text-neutral-600 pl-4">min</p>
-            <h2 className="text-xl">Salary</h2>
+            <h2 className="text-xl">Income</h2>
             <p className="text-neutral-600 pr-4">max</p>
           </div>
           <div className="flex w-[95%] h-16 mt-2 items-center justify-between rounded-3xl bg-thirdColor">
-            <p className="pl-4 text-secondColor">200$</p>
-            <p className="pr-4 text-secondColor">10000$</p>
+            <p className="pl-4 text-secondColor">{minIncome}$</p>
+            <p className="pr-4 text-secondColor">{maxIncome}$</p>
           </div>
           <div className="flex justify-between mt-8 w-[95%]">
             <p className="text-neutral-600 pl-4">min</p>
@@ -107,8 +137,8 @@ const Dashboard = () => {
             <p className="text-neutral-600 pr-4">max</p>
           </div>
           <div className="flex w-[95%] h-16 mt-2 items-center justify-between rounded-3xl bg-thirdColor mb-4">
-            <p className="pl-4 text-red-500">100$</p>
-            <p className="pr-4 text-red-500">5000$</p>
+            <p className="pl-4 text-red-500">{minExpense}$</p>
+            <p className="pr-4 text-red-500">{maxExpense}$</p>
           </div>
         </aside>
       </div>
