8f590ed6a5ca10e2d9d721e233c8bff99c0b54a0
Adrian Fraczek <adrian.frczk01@gmail.com>
2024-10-18 12:24:06 +0200

    add new connection to fetch all incomes, incomes years, and change url based on year value

--- frontend/src/util/http/incomeHttp.jsx
+++ frontend/src/util/http/incomeHttp.jsx
@@ -28,15 +28,16 @@ export async function fetchIncomes({
   year,
   pageNumber,
 }) {
-  const response = await fetch(
-    `http://localhost:8080/expense-tracker/api/v1/incomes/pages/users/${userId}?pageSize=${pageSize}&year=${year}&pageNumber=${pageNumber}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + token,
-      },
-    }
-  );
+  let url = year
+    ? `http://localhost:8080/expense-tracker/api/v1/incomes/pages/users/${userId}?pageSize=${pageSize}&year=${year}&pageNumber=${pageNumber}`
+    : `http://localhost:8080/expense-tracker/api/v1/incomes/pages/users/${userId}?pageSize=${pageSize}&pageNumber=${pageNumber}`;
+
+  const response = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+  });
 
   if (!response.ok) {
     const error = await response.json();
@@ -108,4 +109,46 @@ export async function deleteIncomeById({ id, token }) {
     const error = await response.json();
     throw error.message;
   }
+}
+
+export async function fetchIncomeYears({ token }) {
+  const response = await fetch(
+    `http://localhost:8080/expense-tracker/api/v1/incomes/years`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    const error = await response.json();
+    throw error.message;
+  }
+
+  const result = await response.json();
+
+  return result;
+}
+
+export async function fetchAllIncomes({ userId, token }) {
+  const response = await fetch(
+    `http://localhost:8080/expense-tracker/api/v1/incomes/users/${userId}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    const error = await response.json();
+    throw error.message;
+  }
+
+  const result = await response.json();
+
+  return result;
 }
