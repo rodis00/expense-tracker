8334b1346d54536a79b04c91bbaa71116f1cd275
Adrian Fraczek <adrian.frczk01@gmail.com>
2025-02-07 14:21:49 +0100

    change truncate text to display different title lenght for different devices

--- frontend/src/util/TruncateText.jsx
+++ frontend/src/util/TruncateText.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useMediaQuery } from "@react-hook/media-query";
 
 const TruncateText = ({ text }) => {
-  const maxLength = 30;
+  const isExtraSmallScreen = useMediaQuery(
+    "only screen and (max-width: 449px)"
+  );
+  const isSmallScreen = useMediaQuery(
+    "only screen and (min-width: 450px) and (max-width: 749px)"
+  );
+  const isMediumScreen = useMediaQuery(
+    "only screen and (min-width: 750px) and (max-width: 1369px)"
+  );
+  let maxLength = 0;
+
+  if (isExtraSmallScreen) {
+    maxLength = 16;
+  } else if (isSmallScreen) {
+    maxLength = 20;
+  } else if (isMediumScreen) {
+    maxLength = 25;
+  } else {
+    maxLength = 40;
+  }
 
   function handleTruncateText(text, maxLength) {
     if (text.length > maxLength) {
