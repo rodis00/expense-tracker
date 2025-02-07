c4175627a9fedc4f167eb61f5df710ee419c8286
Adrian Fraczek <adrian.frczk01@gmail.com>
2025-02-07 14:09:21 +0100

    add check expiration of token to root component with modal that show after session is expired

--- frontend/src/components/root-element/RootElement.jsx
+++ frontend/src/components/root-element/RootElement.jsx
@@ -1,10 +1,41 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "../navbar/Navbar";
+import { useDispatch } from "react-redux";
+import { jwtDecode } from "jwt-decode";
+import { authActions } from "../../store/auth-slice";
+import SessionModal from "../../util/SessionModal";
+import { modalActions } from "../../store/modal-slice";
 
 const RootElement = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const token = localStorage.getItem("token");
+      const currentDate = new Date();
+      if (token) {
+        const userId = jwtDecode(token).sub;
+        const exp = jwtDecode(token).exp;
+        const tokenDate = new Date(exp * 1000);
+        if (currentDate > tokenDate) {
+          localStorage.removeItem("token");
+          dispatch(authActions.logout());
+          navigate("/login");
+          dispatch(modalActions.showSessionExpiredInfo());
+        } else {
+          dispatch(authActions.login(userId));
+        }
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
+      <SessionModal />
       <Navbar />
       <Outlet />
     </>
