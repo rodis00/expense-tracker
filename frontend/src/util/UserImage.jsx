d31d333bf2eae5f8769fbb7003f6fe6f33aa2f4f
Adrian Fraczek <adrian.frczk01@gmail.com>
2025-02-23 19:45:16 +0100

    add component that display logo, avatar or image based on delivered data

---
+++ frontend/src/util/UserImage.jsx
@@ -0,0 +1,72 @@
+import { jwtDecode } from "jwt-decode";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+const UserImage = ({ className, image, userImage }) => {
+  const user = useSelector((state) => state.auth.user);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const token = localStorage.getItem("token");
+  const bg = localStorage.getItem("bg");
+  const username = token ? jwtDecode(token).sub : "";
+  const [preview, setPreview] = useState(null);
+  let ObjectURL;
+  let imageURL;
+
+  if (userImage && isAuthenticated) {
+    imageURL = URL.createObjectURL(userImage);
+  }
+
+  useEffect(() => {
+    if (image) {
+      ObjectURL = URL.createObjectURL(image);
+      setPreview(ObjectURL);
+    }
+
+    return () => URL.revokeObjectURL(ObjectURL);
+  }, [image]);
+
+  return (
+    <>
+      {!token && (
+        <div className="flex justify-center">
+          <div
+            className={`flex items-center justify-center rounded-full font-bold text-white bg-secondColor ${className}`}
+          >
+            L
+          </div>
+        </div>
+      )}
+
+      {bg && !image && !userImage && (
+        <div className="flex justify-center">
+          <div
+            className={`flex items-center justify-center rounded-full font-bold text-white ${
+              bg ? "" : "bg-secondColor"
+            } ${className}`}
+            style={{
+              backgroundColor: bg,
+            }}
+          >
+            {username.charAt(0).toUpperCase()}
+          </div>
+        </div>
+      )}
+
+      {bg && !image && userImage && (
+        <img key={user} src={imageURL} className={`${className} bg-main rounded-full`} />
+      )}
+
+      {bg && image && (userImage || !userImage) && (
+        <div className="flex justify-center">
+          <div
+            className={`flex items-center justify-center rounded-full font-bold ${className}`}
+          >
+            <img src={preview} className={` bg-main rounded-full`} />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default UserImage;
