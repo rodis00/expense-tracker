ed72f3e9af84f938abc78e9447445fcd0628c5c1
rodis00 <happyekkov@gmail.com>
2025-02-23 13:20:07 +0100

    remove unnecessary import

--- backend/src/main/java/com/github/rodis00/backend/expense/ExpenseService.java
+++ backend/src/main/java/com/github/rodis00/backend/expense/ExpenseService.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
 
