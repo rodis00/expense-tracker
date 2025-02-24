0f87c5b2a023d9f6bb2a71aaf72b2b9625077b58
Adrian Fraczek <adrian.frczk01@gmail.com>
2025-02-24 17:14:33 +0100

    add new api connection to refresh token, add credentials

--- frontend/src/util/http/auth.jsx
+++ frontend/src/util/http/auth.jsx
@@ -3,6 +3,7 @@ export async function login(values) {
     "http://localhost:8080/expense-tracker/api/v1/auth/authenticate",
     {
       method: "POST",
+      credentials: "include",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
     }
@@ -27,11 +28,34 @@ export async function register(values) {
     "http://localhost:8080/expense-tracker/api/v1/auth/register",
     {
       method: "POST",
+      credentials: "include",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
     }
   );
 
+  if (!response.ok) {
+    const error = await response.json();
+    throw error.message;
+  }
+
+  const result = await response.json();
+
+  if (!result.token) {
+    return null;
+  } else {
+    localStorage.setItem("token", result.token);
+  }
+}
+
+export async function refreshToken() {
+  const response = await fetch(
+    "http://localhost:8080/expense-tracker/api/v1/auth/refresh-token",
+    {
+      method: "POST",
+      credentials: "include",
+    }
+  );
   if (!response.ok) {
     const error = await response.json();
     throw error.message;
